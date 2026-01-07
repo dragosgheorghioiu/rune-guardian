8619880759e600e38e6d0571c0f21feb8a0a613a
DOC <dragos.cristian.odobescu@gmail.com>
2026-01-07 20:14:30 +0200

    fix: better folder structure + conveyor belt

---
+++ Assets/RuneGuardian/Scripts/ChristmasElfScripts/ConveyerBeltLogic/ConveyorController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorController : MonoBehaviour
+{
+    [Header("Material / Shader param")]
+    [SerializeField] private Renderer conveyorRenderer;
+    [SerializeField] private string uvOffsetProperty = "UVOffset";
+    [SerializeField] private float runningSpeed = 1f;
+
+    private MaterialPropertyBlock mpb;
+    private int uvOffsetId;
+
+    private float uvOffset;
+    private bool isRunning = true;
+
+    private void Awake()
+    {
+        if (conveyorRenderer == null)
+            conveyorRenderer = GetComponentInChildren<Renderer>();
+
+        mpb = new MaterialPropertyBlock();
+        uvOffsetId = Shader.PropertyToID(uvOffsetProperty);
+
+        ApplyOffset();
+    }
+
+    private void Update()
+    {
+        if (!isRunning) return;
+
+        uvOffset += runningSpeed * Time.deltaTime;
+        if (uvOffset > 10000f) uvOffset -= 10000f;
+
+        ApplyOffset();
+    }
+
+    public void StartConveyor() => isRunning = true;
+    public void StopConveyor() => isRunning = false;
+
+    public void SetRunningSpeed(float s) => runningSpeed = Mathf.Max(0f, s);
+
+    private void ApplyOffset()
+    {
+        if (conveyorRenderer == null) return;
+
+        conveyorRenderer.GetPropertyBlock(mpb);
+        mpb.SetFloat(uvOffsetId, uvOffset);
+        conveyorRenderer.SetPropertyBlock(mpb);
+    }
+}
---
+++ Assets/RuneGuardian/Scripts/ChristmasElfScripts/ConveyerBeltLogic/SpellProjectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellProjectile : MonoBehaviour
+{
+    public ProjectileType type;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var obj = collision.collider.GetComponentInParent<SpawnedToy>();
+        if (obj != null)
+        {
+            obj.TryHit(type);
+            Destroy(gameObject);
+        }
+
+        
+    }
+
+}
