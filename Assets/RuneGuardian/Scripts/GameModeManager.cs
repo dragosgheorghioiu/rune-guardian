4c4ef8002ee8787d3a8f3ceaf47ed20f0f37c426
Alex-Andrei Cioc <andy.cioc@yahoo.ro>
2026-01-25 22:07:48 +0200

    feat: Projectile moves towards look direction in Grid Mode
    Signed-off-by: Alex-Andrei Cioc <andy.cioc@yahoo.ro>

--- Assets/RuneGuardian/Scripts/GameModeManager.cs
+++ Assets/RuneGuardian/Scripts/GameModeManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using RuneGuardian;
 using UnityEngine;
 
@@ -8,13 +6,16 @@ public class GameModeManager : MonoBehaviour
     [SerializeField] private GameObject conveyorMode;
     [SerializeField] private GameObject gridMode;
 
+    public static GameMode CurrentMode { get; private set; } = GameMode.CONVEYOR_BELT;
+
     public void PickMode(GameMode mode)
     {
+        CurrentMode = mode;
         if (mode == GameMode.CONVEYOR_BELT)
         {
             conveyorMode.SetActive(true);
             gridMode.SetActive(false);
-        } 
+        }
         else
         {
             conveyorMode.SetActive(false);
